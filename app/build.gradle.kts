004c46ab9b17811a171edce77f6029c0c9036caa
Damilola Adeniyi <adeniiyidamilola246@gmail.com>
2024-01-31 11:27:12 +0100

    add dependencies and resources files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
+    id("kotlin-parcelize")
 }
 
 android {
     namespace = "com.example.taxify"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.taxify"
@@ -15,6 +18,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
     }
 
     buildTypes {
@@ -32,12 +44,64 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        languageVersion = "1.8" // data objects
     }
 }
 
 dependencies {
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+    val simplestack_version = "2.2.5"
+
+    implementation("com.github.Zhuinden.simple-stack-extensions:core-ktx:$simplestack_version")
+    implementation("com.github.Zhuinden.simple-stack-extensions:fragments:$simplestack_version")
+    implementation("com.github.Zhuinden.simple-stack-extensions:fragments-ktx:$simplestack_version")
+    implementation("com.github.Zhuinden.simple-stack-extensions:navigator-ktx:$simplestack_version")
+    implementation("com.github.Zhuinden.simple-stack-extensions:services:$simplestack_version")
+    implementation("com.github.Zhuinden.simple-stack-extensions:services-ktx:$simplestack_version")
+
+    implementation("com.github.skydoves:landscapist-glide:2.1.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
+    implementation("androidx.compose.ui:ui-tooling:1.6.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
+    implementation("androidx.compose.foundation:foundation:1.6.0")
+    implementation("androidx.compose.material:material:1.6.0")
+    implementation("androidx.compose.material:material-icons-core:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+
+
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    val stream_version = "5.11.10"
+    implementation("io.getstream:stream-chat-android-client:$stream_version")
+    implementation("io.getstream:stream-chat-android-ui-components:$stream_version")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+    implementation("com.google.android.libraries.places:places:3.3.0")
+    implementation("com.google.maps:google-maps-services:2.1.2")
+    implementation("com.google.maps.android:android-maps-utils:3.3.0")
+    implementation("org.slf4j:slf4j-simple:1.7.25")
+
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
